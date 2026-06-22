422f8ff3c3c1d2379a1e874baf4bc42793fd3307
annemone07 <rodrigomachadoaraujo00@gmail.com>
2026-06-21 22:49:35 -0300

    base da musica de fundo e do arquivo config adicionados

---
+++ config.py
@@ -0,0 +1,19 @@
+import pygame
+import os
+
+#se for mover o deltaTime pra ca alterar todos as variaveis do mainCode pra config.variavel
+
+pygame.init()
+folderPath = os.path.dirname(os.path.abspath(__file__))
+camera = pygame.math.Vector2(0, -6)
+bgHeight = pygame.display.Info().current_h
+bgWidth = pygame.display.Info().current_w
+#tamanhoTela:tuple = pygame.display.get_desktop_sizes()[0]
+telaSizePlaceholder = (bgWidth,bgHeight)
+tela = pygame.display.set_mode(telaSizePlaceholder)
+fonte = pygame.font.SysFont("arial", 40, True, False)
+fonte_grande = pygame.font.SysFont("arial", 100, True, False)
+fps=60
+
+#configs de sons e musicas
+musicaIsPaused = False
---
+++ funcoes.py
@@ -0,0 +1,50 @@
+import pygame
+import os
+import config
+from player import Jogador
+
+pygame.init()
+pygame.mixer.init()
+pygame.mixer.set_num_channels(32)
+musica = (os.path.join(config.folderPath,"sons","musicas","Flying_me_softly.ogg"))
+pygame.mixer.music.load(musica)
+
+musicaIsPaused = False
+
+def criarJogador(deltaTime):
+    jogadorCriado = Jogador(
+        spriteImage=os.path.join(config.folderPath,'images', 'playerSprites', 'slime_green.png'),
+        posInicial=(config.bgWidth / 2, config.bgHeight-300),
+        dt=deltaTime,
+        tamanhoMapa=(config.bgWidth,config.bgHeight)
+        #grupos=self.all_sprites,
+        #game=self
+    )
+    return jogadorCriado
+
+def resetarVariaveis(grupoGrupos):
+    grupoItem, grupoEscudo, grupoQuickShot, grupoBulletTime, grupoCura, grupoMoeda, grupoJogador, grupoRastro, grupoBala, grupoInimigo, grupoBullets = grupoGrupos
+    grupoItem.empty()
+    grupoEscudo.empty()
+    grupoQuickShot.empty()
+    grupoBulletTime.empty()
+    grupoCura.empty()
+    grupoMoeda.empty()
+    grupoJogador.empty()
+    grupoRastro.empty()
+    grupoBala.empty()
+    grupoInimigo.empty()
+    grupoBullets.empty()
+    
+def sons(evento):
+    if evento=="jogando":
+        if not config.musicaIsPaused:
+            pygame.mixer.music.play(-1)
+        else:
+            config.musicaIsPaused=False
+            pygame.mixer.music.unpause()
+    elif evento=="pausado":
+        config.musicaIsPaused=True
+        pygame.mixer.music.pause()
+    else:
+        pygame.mixer.music.fadeout(200)
