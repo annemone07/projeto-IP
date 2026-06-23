c60a4383ce149f490f976adb7fea7f3cb30e255c
Rafaelrtal <rtal@cin.ufpe.br>
2026-06-23 18:00:04 -0300

    Sprites novos do player

--- funcoes.py
+++ funcoes.py
@@ -13,7 +13,7 @@ musicaIsPaused = False
 
 def criarJogador(deltaTime):
     jogadorCriado = Jogador(
-        spriteImage=os.path.join(config.folderPath,'images', 'playerSprites', 'Main Ship - Base - Full health.png.png'),
+        spriteImage=os.path.join(config.folderPath,'images', 'playerSprites', 'spritesheet_player_spaceship_up1.png'),
         posInicial=(config.bgWidth / 2, config.bgHeight-300),
         dt=deltaTime,
         tamanhoMapa=(config.bgWidth,config.bgHeight)
