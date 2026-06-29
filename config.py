8c77efc955938102343371c0e2a9cd147f86bc8e
annemone07 <rodrigomachadoaraujo00@gmail.com>
2026-06-28 23:39:39 -0300

    tela opções com volume e resolução

--- config.py
+++ config.py
@@ -10,11 +10,13 @@ bgHeight = pygame.display.Info().current_h
 bgWidth = pygame.display.Info().current_w
 #tamanhoTela:tuple = pygame.display.get_desktop_sizes()[0]
 telaSizePlaceholder = (bgWidth,bgHeight)
-tela = pygame.display.set_mode(telaSizePlaceholder)
+os.environ['SDL_VIDEO_CENTERED'] = '1'
+tela = pygame.display.set_mode(telaSizePlaceholder, pygame.RESIZABLE, display=0)
 fonte = pygame.font.SysFont("arial", 40, True, False)
 fonte_grande = pygame.font.SysFont("arial", 100, True, False)
 fonte_media = pygame.font.SysFont("arial", 65, True, False)
 fps=60
+volume = 1.0
 
 #configs de sons e musicas
 musicaIsPaused = False
